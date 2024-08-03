e1e662c7830f3a1d025808a81f0884d9c7af8109
James Jeon <kor3a5@gmail.com>
2024-08-02 19:47:44 -0700

    Initial Commit

---
+++ Geolocation_v1.0.0/Geolocation_v1_0_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Geolocation_v1_0_0App.swift
+//  Geolocation_v1.0.0
+//
+//  Created by Subong Jeon on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Geolocation_v1_0_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
