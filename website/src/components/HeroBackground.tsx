ff1aec06cd22f7d5a5842ee3c08a8e3776bee894
jjeon <jjeon@nbpcorp.com>
2026-05-20 09:35:56 -0700

    Realign hero phone navigation route

--- website/src/components/HeroBackground.tsx
+++ website/src/components/HeroBackground.tsx
@@ -11,7 +11,7 @@ function RouteOverlay({
   compact?: boolean;
 }) {
   const route = compact
-    ? "M493 632 L493 548 L594 548 L594 412 L590 394 L590 254 L662 254"
+    ? "M548 632 L548 588 L638 588 L638 412 L634 394 L634 220 L662 220"
     : "M463 632 L463 540 L576 540 L576 412 L573 394 L573 215 L610 215";
   const pin = compact ? "translate(662 215)" : "translate(610 206)";
 
