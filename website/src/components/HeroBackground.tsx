cb8c5fee475f4db1230da5a6353c210b89633305
jjeon <jjeon@nbpcorp.com>
2026-05-20 10:11:38 -0700

    Fine tune hero phone route alignment

--- website/src/components/HeroBackground.tsx
+++ website/src/components/HeroBackground.tsx
@@ -11,7 +11,7 @@ function RouteOverlay({
   compact?: boolean;
 }) {
   const route = compact
-    ? "M508 632 L508 588 L600 588 L600 412 L596 394 L596 220 L662 220"
+    ? "M526 632 L526 588 L616 588 L616 412 L612 394 L612 220 L662 220"
     : "M463 632 L463 540 L576 540 L576 412 L573 394 L573 215 L610 215";
   const pin = compact ? "translate(662 215)" : "translate(610 206)";
 
